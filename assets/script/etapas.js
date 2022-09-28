2e73b2219e31d913f6ee91a92f39580134133f1e
unknown <gilberto.santos.96@gmail.com>
2022-09-28 18:11:01 -0300

    primeiro commit

---
+++ assets/script/etapas.js
@@ -0,0 +1,72 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'11111',
+                nome:'Kladileusa',
+                partido: 'PJ',
+                fotos:[
+                    {url:'deputada1.jpg', legenda: 'Deputado(a)'}
+                ]
+            },
+            {
+                numero:'22222',
+                nome:'Josinelson',
+                partido: 'PW',
+                fotos:[
+                    {url:'deputado2.jpg', legenda: 'Deputado(a)'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '111',
+                nome: 'Alex Molin',
+                partido: 'PJ',
+                fotos:[
+                    {url:'senador1.jpg', legenda: 'Senador'},
+                ]
+            },
+            {
+                numero: '222',
+                nome: 'Homérico Jogador',
+                partido: 'PW',
+                fotos:[
+                    {url:'senador2.jpg', legenda: 'Senador'},
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Lólia da Silva',
+                partido: 'PJ',
+                vice:'Janira Sandália',
+                fotos:[
+                    {url:'candidato_03_13.jpg', legenda: 'Presidente'},
+                    {url:'candidata_03_13_vice.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Ladir Bizolório',
+                partido: 'PW',
+                vice:'José Rachadas',
+                fotos:[
+                    {url:'candidato_01_22.jpg', legenda: 'Presidente'},
+                    {url:'candidato_01_22_vice.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]
+    }
+]
